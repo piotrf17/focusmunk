05750edfe5a59a60c0c7e64425245ddcfa1aff81
Piotr Fidkowski <piotr@fidkowski.com>
2022-10-21 23:13:20 -0700

    Update for manifest v3.

--- extension/background.js
+++ extension/background.js
@@ -1,7 +1,3 @@
-// Global variables.
-var denylist = [];
-var allowlist = [];
-
 // Reload regexes from raw textbox.
 function reloadRegexes(raw) {
   var regexes = raw.split('\n');
@@ -10,6 +6,31 @@ function reloadRegexes(raw) {
   }
   return regexes;
 }
+
+// Global variables, these will be loaded at first startup of the
+// service worker.
+let allowlist;
+let denylist;
+let starting = chrome.storage.sync.get().then(data => {
+  console.log(data);
+  denylist = reloadRegexes(data.denylist);
+  allowlist = reloadRegexes(data.allowlist);
+  console.log('Initial denylist: ', denylist);
+  console.log('Initial allowlist: ', allowlist);
+  starting = null;
+});
+
+// Listen for updates to the global variables.
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  if ('denylist' in changes) {
+    console.log('denylist changed:', changes['denylist']);
+    denylist = reloadRegexes(changes['denylist'].newValue);
+  }
+  if ('allowlist' in changes) {
+    console.log('allowlist changed:', changes['allowlist']);
+    allowlist = reloadRegexes(changes['allowlist'].newValue);
+  }
+});
 
 function isBlocked(url) {
   for (var i = 0; i < allowlist.length; ++i) {
@@ -28,30 +49,13 @@ function isBlocked(url) {
 }
 
 // Listen for page loading updates.
-chrome.tabs.onUpdated.addListener(function(tabId, change, tab) {
+chrome.tabs.onUpdated.addListener(async function(tabId, change, tab) {
   if (change.status == 'loading' && typeof change.url != 'undefined') {
+    // Wait for allow/deny lists, if we're starting up.
+    if (starting) await starting;
+
     if (isBlocked(change.url)) {
-      chrome.tabs.update(tabId, {url: chrome.extension.getURL('blocked.html')});
+      chrome.tabs.update(tabId, {url: 'blocked.html'});
     }
   }
 });
-
-// Listen for updates to the regex list.
-chrome.storage.onChanged.addListener(function(changes, namespace) {
-  if ('denylist' in changes) {
-    console.log('denylist changed:', changes['denylist']);
-    denylist = reloadRegexes(changes['denylist'].newValue);
-  }
-  if ('allowlist' in changes) {
-    console.log('allowlist changed:', changes['allowlist']);
-    allowlist = reloadRegexes(changes['allowlist'].newValue);
-  }
-});
-
-// Setup first set of regexes.
-chrome.storage.sync.get(
-  {denylist: '', allowlist: ''}, function(items) {
-    reloadRegexes(items.denylist, denylist);
-    reloadRegexes(items.allowlist, allowlist);
-  });
-
